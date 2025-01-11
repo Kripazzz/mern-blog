6224a7a27a58e870773ee27e9fd8b68843a6033a
kripa <164274144+Kripazzz@users.noreply.github.com>
2025-01-11 16:20:46 +0530

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Label, TextInput, Button, Alert, Spinner} from 'flowbite-react';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
-function SignIn() {
+export default function SignIn() {
 
   const [formData, setFormData] = useState({});    
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error: errorMessage} = useSelector(state => state.user); 
+  const dispatch =  useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -18,11 +20,10 @@ function SignIn() {
     e.preventDefault();
 
     if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
@@ -31,16 +32,16 @@ function SignIn() {
       const data = await res.json();
 
       if(data.success === false) {
-        return setErrorMessage(data.message);
+       dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+      // setLoading(false);
 
       if(res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
       
       
     }
@@ -123,5 +124,3 @@ function SignIn() {
     </div>
   )
 }
-
-export default SignIn;
